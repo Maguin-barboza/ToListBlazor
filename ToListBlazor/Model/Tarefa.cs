8d9c35b932108c10078ac3b5e206ed801b249075
magno_barboza@hotmail.com <magno_barboza@hotmail.com>
2022-01-06 18:43:05 -0300

    Inicio do projeto.

---
+++ ToListBlazor/Model/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿namespace ToListBlazor.Model
+{
+    public class Tarefa
+    {
+        public string Descricao { get; set; }
+        public TimeOnly TempoEstimado { get; set; } = new TimeOnly();
+
+
+        public static List<Tarefa> Tarefas = new List<Tarefa>();
+
+        public static void AddTarefa(Tarefa tarefa)
+        {
+            Tarefas.Add(tarefa);
+        }
+
+        public static void RemoveTarefa(Tarefa tarefa)
+        {
+            Tarefas.Remove(tarefa);
+        }
+    }
+}
